edea0789d4a8221011549f6bd6db06955dedaea1
megansimmons <simmons.megan584@gmail.com>
2019-04-21 13:27:42 -0500

    comments and string format added to JDBCTest

--- src/main/java/JDBCTest.java
+++ src/main/java/JDBCTest.java
@@ -19,14 +19,18 @@ public class JDBCTest {
             //variable types from mySQL have to be converted to comparable types in java
             //do not need to include the auto_increment id value becuase it will generate on its own
 
+            //wasn't allowed access as user 'root'@'localhost' -->no password associated with root so not sure if that's the problem
             String username = "kkmarie";
             String email = "kkmarie987@gmail.com";
             String password = "kkpassword";
 
-            stmt.executeUpdate("INSERt INTO users(username, email, password) values('"+username+"', '"+email+"', '"+password+"')");
+            stmt.executeUpdate("INSERT INTO users(username, email, password) values('"+username+"', '"+email+"', '"+password+"')");
+            //could aslo do the same thing with the following
+            stmt.executeUpdate(String.format("INSERT INTO users(username, email, password,) values('%s', '%s', '%s')", username, email, password));
+
 
             ResultSet rs = stmt.executeQuery("SELECT * FROM users");
-
+            //use .next() to move cursor through results row by row, will return true as long as more
             while(rs.next()){
                 //now we can include the id if we want to see it printed out
                 System.out.println("id in table " + rs.getInt("id"));
