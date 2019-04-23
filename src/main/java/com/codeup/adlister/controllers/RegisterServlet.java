fb3fb261a9bd4684ae6b47be1638e055f1a3239c
megansimmons <simmons.megan584@gmail.com>
2019-04-23 13:41:49 -0500

    password hash

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -2,6 +2,7 @@ package com.codeup.adlister.controllers;
 
 import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.models.User;
+import org.mindrot.jbcrypt.BCrypt;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -19,14 +20,20 @@ public class RegisterServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         String email = request.getParameter("email");
-        String password = request.getParameter("password");
-        String passwordConfirmation = request.getParameter("confirm_password");
+
+//        String password = request.getParameter("password")
+                ;
+        String password = BCrypt.hashpw(request.getParameter("password"), BCrypt.gensalt());
+
+        Boolean passwordConfirmation = BCrypt.checkpw(request.getParameter("confirm_password"), password);
+
 
         // validate input
+
         boolean inputHasErrors = username.isEmpty()
             || email.isEmpty()
             || password.isEmpty()
-            || (! password.equals(passwordConfirmation));
+            || (! passwordConfirmation);
 
         if (inputHasErrors) {
             response.sendRedirect("/register");
@@ -34,6 +41,7 @@ public class RegisterServlet extends HttpServlet {
         }
 
         // create and save a new user
+
         User user = new User(username, email, password);
         DaoFactory.getUsersDao().insert(user);
         response.sendRedirect("/login");
